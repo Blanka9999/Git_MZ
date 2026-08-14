6218da950658415fbc33b819411f4d307b7e0f69
blanka <albiniblanka@gmail.com>
2026-08-14 20:01:16 +0200

    localstorage-saving/fix table visual issues

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ fetch("https://api.siposm.hu/word")
         words.forEach(word => {
             const row = document.createElement("tr");
             const data = document.createElement("td");
+            const buttonCell = document.createElement("td")
             const saveButton = document.createElement("button")
 
             data.textContent = word;
             saveButton.textContent = "Save";
             saveButton.type = "button";
             saveButton.className = "btn btn-primary my-2";
+            row.className = "border-1"
 
             row.appendChild(data);
             row.appendChild(saveButton);
-            tableBody.appendChild(row);
+            wordTable.appendChild(row);
         });
     })
     .catch(error => {
