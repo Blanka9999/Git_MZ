6e03744e8058959a475c6078b5293e87c8d81f71
blanka <albiniblanka@gmail.com>
2026-08-14 20:21:39 +0200

    localstorage-saving/created local storage, added word saving function with word check

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ fetch("https://api.siposm.hu/word")
     .then(words => {
         const tableBody = document.querySelector("#wordTable tbody");
 
+        let savedList = JSON.parse(localStorage.getItem("savedList")) || [];
+
         words.forEach(word => {
             const row = document.createElement("tr");
             const data = document.createElement("td");
@@ -15,6 +17,13 @@ fetch("https://api.siposm.hu/word")
             saveButton.type = "button";
             saveButton.className = "btn btn-primary my-2";
             row.className = "border-1"
+
+            saveButton.addEventListener("click", function () {
+                if (!savedList.includes(word)) {
+                savedList.push(word);
+                localStorage.setItem("savedList", JSON.stringify(savedList));
+                }
+            });
 
             row.appendChild(data);
             row.appendChild(saveButton);
